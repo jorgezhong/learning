2db712b436a292d95929bdf1cba122a81b29dbcd
jorgezhong <zhongyuxian@Oeasy.com>
2019-09-11 17:52:14 +0800

    - AQS

---
+++ concurrent-example/src/com/jorgezhong/concurrent/AQS/Demo.java
@@ -0,0 +1,12 @@
+package com.jorgezhong.concurrent.AQS;
+
+/**
+ * AQS:AbstractQueuedSynchronizer类
+ * - 是一个抽象类，通过它实现锁，或者其他同步组件才有意义
+ * - 相当于一个模板方法模式
+ *
+ * Project <learning>
+ * Created by jorgezhong on 2019/9/11 16:39.
+ */
+public class Demo {
+}
