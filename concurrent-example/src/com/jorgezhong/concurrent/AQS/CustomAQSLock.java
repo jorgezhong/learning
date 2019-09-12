2d9d17712d123f3d20bc2abae8aa715d9dca96e6
jorgezhong <zhongyuxian@Oeasy.com>
2019-09-12 18:23:04 +0800

    - AQS理解 - ReentrantLock非公平锁加锁解锁过程

---
+++ concurrent-example/src/com/jorgezhong/concurrent/AQS/CustomAQSLock.java
@@ -0,0 +1,20 @@
+package com.jorgezhong.concurrent.AQS;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ *
+ * 通过继承AQS实现自定义锁
+ * Project <learning>
+ * Created by jorgezhong on 2019/9/12 17:33.
+ */
+public class CustomAQSLock {
+
+
+    private class Sync extends AbstractQueuedSynchronizer{
+
+    }
+
+
+
+}
