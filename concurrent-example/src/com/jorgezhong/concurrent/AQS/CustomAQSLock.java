1be1828e0593942dcee89c60076843de0fa8c322
jorgezhong <zhongyuxian@Oeasy.com>
2019-09-16 14:15:52 +0800

    自定义AQS锁 - 自定义AQS锁的实现 - 锁的可重入实现

--- concurrent-example/src/com/jorgezhong/concurrent/AQS/CustomAQSLock.java
+++ concurrent-example/src/com/jorgezhong/concurrent/AQS/CustomAQSLock.java
@@ -1,20 +1,117 @@
 package com.jorgezhong.concurrent.AQS;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 /**
- *
  * 通过继承AQS实现自定义锁
  * Project <learning>
  * Created by jorgezhong on 2019/9/12 17:33.
  */
-public class CustomAQSLock {
+public class CustomAQSLock implements Lock {
 
 
-    private class Sync extends AbstractQueuedSynchronizer{
+    private Sync sync = new Sync();
 
+    private class Sync extends AbstractQueuedSynchronizer {
+
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+
+
+            /**
+             * 考虑的问题：
+             * 1.如果第一个线程进来，可以拿到锁，因此我们可以返回true
+             * 2.如果第二个线程进来，拿不到锁，阻塞，返回false
+             *  - 如果当前进来的线程和当前保存的线程同一个线程，则允许拿到锁（锁重入）
+             *  - 此时需要更新state值
+             * 3.如何判断是第一个线程进来，还是第二个，还是其他线程进来
+             *
+             * 通过 AQS 提供的state属性表示线程的状态
+             * 通过 AQS 提供的exclusiveOwnerThread属性表示独占锁的线程
+             * 通过 AQS 提供的CAS自旋方法，修改状态
+             */
+
+
+            Thread currentThread = Thread.currentThread();
+            int state = getState();
+
+            if (state == 0) {
+                if (compareAndSetState(0, arg)) {
+                    setExclusiveOwnerThread(currentThread);
+                    return true;
+                }
+            }else if(currentThread == getExclusiveOwnerThread()){
+                //锁重入逻辑
+                setState(state + 1);
+                return true;
+            }
+            return false;
+
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+
+            /**
+             * 考虑的问题：
+             * 1.锁的获取和释放是一一对应的，那么调用此方法的线程一定是当前线程
+             */
+
+            if (Thread.currentThread() != getExclusiveOwnerThread()) {
+                throw new RuntimeException();
+            }
+            //支持锁重入
+            int state = getState() - arg;
+
+            boolean flag = false;
+
+            if (state == 0) {
+                flag = true;
+                setExclusiveOwnerThread(null); //help GC
+            }
+            //支持锁重入
+            setState(state);
+            return flag;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
     }
 
 
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
 
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
 }
+
--- concurrent-example/src/com/jorgezhong/concurrent/AQS/Demo.java
+++ concurrent-example/src/com/jorgezhong/concurrent/AQS/Demo.java
@@ -4,37 +4,93 @@ package com.jorgezhong.concurrent.AQS;
  * AQS:AbstractQueuedSynchronizer类
  * - 是一个抽象类，通过它实现锁，或者其他同步组件才有意义
  * - 相当于一个模板方法模式
- *
+ * <p>
  * AQS
- *  AbstractQueuedSynchronizer的父类AbstractOwnableSynchronizer持有一个独占线程exclusiveOwnerThread，存储当前拿到锁的线程
- *  AbstractQueuedSynchronizer的持有一个成员变量state,用于记录是否获取锁，以及重入锁的次数
- *  AQS模板定义了共享模式和独占模式，上述两点是基于独占模式阐述的。
- *  AQS定义的抽象模板方法,需要是否是独占模式来去实现它，才有意义
- *      - boolean tryAcquire(int arg)
- *      - boolean tryRelease(int arg)
- *      - int tryAcquireShared(int arg)
- *      - boolean tryReleaseShared(int arg)
- *      - boolean isHeldExclusively()
-
- *  ReentrantLock 使用了AQS 其内部类Sync继承了AQS，分别实现类公平以及非公平的锁
- *
- *  非公平举例加锁：
- *      - 获取当前线程，获取state对比
- *      - state = 0 则通过CAS（自旋）将state设置为1，并且设置exclusiveOwnerThread为当前线程，表示独占，获取锁成功
- *      - state != 0 则通过exclusiveOwnerThread和当前线程判断是否是同一个，同一个则表示锁重入，设置state + 1，获取锁成功
- *
- *  非公平举例释放锁：
- *      - 获取 state - 1
- *      - 获取 exclusiveOwnerThread 并判断是否为当前线程（不是当前线程没有意义，抛出异常）
- *      - 判断之前算的 state - 1 == 0 如实是，则表示没有释放最外层锁（无重入锁了），释放成功
- *      - 设置 exclusiveOwnerThread = null
- *      - 设置 state = 0
- *
- *
+ * AbstractQueuedSynchronizer的父类AbstractOwnableSynchronizer持有一个独占线程exclusiveOwnerThread，存储当前拿到锁的线程
+ * AbstractQueuedSynchronizer的持有一个成员变量state,用于记录是否获取锁，以及重入锁的次数
+ * AQS模板定义了共享模式和独占模式，上述两点是基于独占模式阐述的。
+ * AQS定义的抽象模板方法,需要是否是独占模式来去实现它，才有意义
+ * - boolean tryAcquire(int arg)
+ * - boolean tryRelease(int arg)
+ * - int tryAcquireShared(int arg)
+ * - boolean tryReleaseShared(int arg)
+ * - boolean isHeldExclusively()
+ * <p>
+ * ReentrantLock 使用了AQS 其内部类Sync继承了AQS，分别实现类公平以及非公平的锁
+ * <p>
+ * 非公平举例加锁：
+ * - 获取当前线程，获取state对比
+ * - state = 0 则通过CAS（自旋）将state设置为1，并且设置exclusiveOwnerThread为当前线程，表示独占，获取锁成功
+ * - state != 0 则通过exclusiveOwnerThread和当前线程判断是否是同一个，同一个则表示锁重入，设置state + 1，获取锁成功
+ * <p>
+ * 非公平举例释放锁：
+ * - 获取 state - 1
+ * - 获取 exclusiveOwnerThread 并判断是否为当前线程（不是当前线程没有意义，抛出异常）
+ * - 判断之前算的 state - 1 == 0 如实是，则表示没有释放最外层锁（无重入锁了），释放成功
+ * - 设置 exclusiveOwnerThread = null
+ * - 设置 state = 0
+ * <p>
+ * <p>
  * Project <learning>
  * Created by jorgezhong on 2019/9/11 16:39.
  */
 public class Demo {
 
+    private int value;
+
+    private CustomAQSLock lock = new CustomAQSLock();
+
+    public int next() {
+        lock.lock();
+        try {
+            Thread.sleep(300);
+            return value++;
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    public void a() {
+        lock.lock();
+        System.out.println("a");
+        b();
+        lock.unlock();
+    }
+
+    public void b() {
+        lock.lock();
+        System.out.println("b");
+        lock.unlock();
+    }
+
+    public static void main(String[] args) {
+//        customAQSLockTest();
+        reentrantCustomLockTest();
+
+    }
+
+    private static void reentrantCustomLockTest() {
+        Demo demo = new Demo();
+        Runnable runnable = demo::a;
+
+        new Thread(runnable).start();
+    }
+
+    private static void customAQSLockTest() {
+        Demo demo = new Demo();
+
+        Runnable runnable = () -> {
+            while (true) {
+                System.out.println(Thread.currentThread().getId() + ":" + demo.next());
+            }
+
+        };
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+    }
 
 }
